22e9dc7b322bd38122ecb0b3722e7acc27601275
CostelDobrica <dobre.1999@yahoo.com>
2024-07-19 23:00:54 +0430

    Project-structure and products

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Swiper from "swiper";
+import 'swiper/swiper-bundle.css';
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: 'swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
