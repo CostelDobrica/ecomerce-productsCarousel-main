918386d12fdbc3bd92c99602b023a83d37c33590
CostelDobrica <dobre.1999@yahoo.com>
2024-07-20 17:08:52 +0430

    Html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import Swiper from "swiper";
-import 'swiper/swiper-bundle.css';
+import "swiper/swiper-bundle.css";
+import products from "./products.json";
+
+
+const swiperContainer = document.querySelector('.swiper-wrapper');
+
+products.forEach(product => {
+  const slide = document.createElement('div');
+  slide.classList.add('swiper-slide');
+  slide.innerHTML = `
+    <img src="${product.image}" alt="${product.name}">
+    <div class="product-name">${product.name}</div>
+    <div class="product-price">$${product.price}</div>
+    <div class="product-discounted-price">$${product.discountedPrice}</div>
+    <button class="action-button">Add to Cart</button>
+  `;
+  swiperContainer.appendChild(slide);
+});
 
 const swiper = new Swiper('.swiper-container', {
     loop: true,
